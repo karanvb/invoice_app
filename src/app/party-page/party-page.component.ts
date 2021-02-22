ade7e3e9cb8621210ecb7fa9d4f77be3f4b96ecb
Karan V B <karanvb@outlook.com>
2021-02-22 11:37:13 +0530

    v1

---
+++ src/app/party-page/party-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-page',
+  templateUrl: './party-page.component.html',
+  styleUrls: ['./party-page.component.css']
+})
+export class PartyPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
