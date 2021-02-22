34758c586ac92ff2a8ad03754341307a1b38422a
Karan V B <karanvb@outlook.com>
2021-02-22 17:08:31 +0530

    check

---
+++ src/app/model/partyModel.ts
@@ -0,0 +1,5 @@
+export class Party {
+    constructor(public id?: string, public partyName?: string, public partyType?: string, public phoneNumber?: string, public city?: string, public state?: string, public postalCode?: string) { }
+}
+
+
---
+++ src/app/model/productModel.ts
@@ -0,0 +1,3 @@
+export class Product {
+    constructor(public id?: string, public itemName?: string, public itemType?: string, public salesPrice?: number, public purchasePrice?: number, public hsn?: string, public gst?: number) { }
+}
